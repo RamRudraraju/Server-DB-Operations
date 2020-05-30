2c7e969e73dccfc39f03e16c80fb7547f4a99410
Rama krishna Raju Rudraraju <rrkr2016@uab.edu>
2020-05-30 15:10:53 -0500

    MySQL Database Creation

---
+++ DB_creation.js
@@ -0,0 +1,28 @@
+var mysql = require('mysql');
+
+// Connecting to MySQL using NodeJS
+console.log("mysql DB Connection");
+
+
+// Creating a connection. 
+var con = mysql.createConnection({ // (host, port, user, password) are the default values we got from MAMP Server in Local.
+    host: "localhost", 
+    port: 8889,
+    user: "root",
+    password: "root"
+
+})
+
+// Connecting to MySQL
+con.connect(function(err){
+   if(err) throw err;
+
+   console.log("Connected to Database !!!");
+
+   con.query("CREATE DATABASE nodeDB", function(err, result){
+       if(err) throw err;
+
+       console.log("Database Created !");
+   });  
+
+})
